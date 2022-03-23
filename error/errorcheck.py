28c03a268c77376849777a30382041d8165ea931
inha <Nuvo>
2022-03-23 16:55:02 +0900

    gps error check update

--- error/errorcheck.py
+++ error/errorcheck.py
@@ -5,7 +5,7 @@ import sys
 
 from sensor_msgs.msg import CompressedImage, PointCloud2, Temperature
 from visualization_msgs.msg import MarkerArray
-from sbg_driver.msg import SbgEkfNav, SbgEkfEuler
+from sbg_driver.msg import SbgEkfNav, SbgEkfEuler, SbgGpsPos
 from std_msgs.msg import Int8MultiArray, Float32MultiArray
 # kana modify
 from geometry_msgs.msg import Point
@@ -18,25 +18,26 @@ class Final:
         rospy.Subscriber('/gmsl_camera/dev/video0/compressed', CompressedImage, self.video0_Callback)
         rospy.Subscriber('/gmsl_camera/dev/video1/compressed', CompressedImage, self.video1_Callback)
         rospy.Subscriber('/os_cloud_node/points', PointCloud2, self.LiDAR_points_Callback)
-        rospy.Subscriber('/imu/temp', Temperature, self.IMU_temp_Callback)
         rospy.Subscriber('/sbg/ekf_euler', SbgEkfEuler, self.sbg_ekf_Callback)
+        rospy.Subscriber('/sbg/gps_pos', SbgGpsPos, self.GPS_pos_Callback)
+        rospy.Subscriber('/sbg/ekf_nav', SbgEkfNav, self.EKF_nav_Callback)
         rospy.Subscriber('/Camera/Front60/od_bbox', Float32MultiArray, self.Video0_result_Callback)
         rospy.Subscriber('/pillar_marker_array', MarkerArray, self.LiDAR_result_Callback)
         #kana modify
-        rospy.Subscriber('/gps_accuracy', Point, self.GPS_Acc_Callback)
-
+        self.gps_check = rospy.Publisher('/gps_accuracy', Point,  queue_size=1)
         self.sensor_check = rospy.Publisher('/sensor_state', Int8MultiArray, queue_size = 1)
         self.system_check = rospy.Publisher('/system_state', Int8MultiArray, queue_size = 1)
 
         self.Video0_count = 0
         self.Video1_count = 0
         self.LiDAR_points_count = 0
-        self.IMU_temp_count = 0
         self.Sbg_euler_count = 0
         self.Video0_result_count = 0
         self.LiDAR_result_count = 0
         #kana modify
         self.GPS_error_type = 0
+        self.FIX_error_type = 0
+        self.GPS_accuracy = 0
 
     def video0_Callback(self, msg):
         self.Video0_count += 1
@@ -47,32 +48,26 @@ class Final:
     def LiDAR_points_Callback(self, msg):
         self.LiDAR_points_count += 1
 
-    def IMU_temp_Callback(self, msg):
-        self.IMU_temp_count += 1
-
     def sbg_ekf_Callback(self, msg):
         self.Sbg_euler_count += 1
+
+    def GPS_pos_Callback(self, msg):
+        self.FIX_error_type = 1 if msg.diff_age > 500 else 0
+
+    def EKF_nav_Callback(self, msg):
+        self.GPS_error_type = 1 if msg.position_accuracy.x > 0.7 else 0
+        self.GPS_accuracy = int(msg.position_accuracy.x * 100 )
 
     def Video0_result_Callback(self, msg):
         self.Video0_result_count += 1
 
     def LiDAR_result_Callback(self, msg):
-        self.LiDAR_result_count += 1
-
-    #kana modify
-    def GPS_Acc_Callback(self, msg):
-        if(msg.x == 0):
-            self.GPS_error_type = 1
-        elif(msg.y < 70):
-            self.GPS_error_type = 2
-        elif(msg.x == 1 and msg.y >= 70):
-            self.GPS_error_type = 0
-
-    
+        self.LiDAR_result_count += 1    
 
     def checker(self):
         sensor_state = Int8MultiArray() 
         system_state = Int8MultiArray()
+        gps_accuracy = Point()
         print("="*50)
 
         if self.Video0_count > 23:
@@ -96,27 +91,21 @@ class Final:
 
         print("LiDAR : {}Hz".format(self.LiDAR_points_count))
 
-        if self.IMU_temp_count > 18:
+        if self.GPS_error_type == 0 or self.FIX_error_type == 0:
             sensor_state.data.extend([False])
         else:
             sensor_state.data.extend([True])
 
-        print("IMU : {}Hz".format(self.IMU_temp_count))
+        #print("GPS : {}Hz".format(self.IMU_temp_count))
 
-        if self.Sbg_euler_count > 18 and self.GPS_error_type == 0:
-            sensor_state.data.extend([False])
-        elif self.Sbg_euler_count <= 18 or self.GPS_error_type > 0:
-            sensor_state.data.extend([True])
+        gps_accuracy.x = self.GPS_error_type
+        gps_accuracy.y = self.GPS_accuracy
 
-        '''
-        #kana modify
-        if self.GPS_error_type == 0 :
+
+        if self.Sbg_euler_count > 18 :
             sensor_state.data.extend([False])
-        else:
+        elif self.Sbg_euler_count <= 18 :
             sensor_state.data.extend([True])
-        '''
-        print("GPS Accuracy Error Type : {}".format(self.GPS_error_type))
-        
 
         print("INS : {}Hz".format(self.Sbg_euler_count))
 
@@ -140,18 +129,18 @@ class Final:
         self.Video0_count = 0
         self.Video1_count = 0
         self.LiDAR_points_count = 0
-        self.IMU_temp_count = 0
         self.Sbg_euler_count = 0
         self.Video0_result_count = 0
         self.LiDAR_result_count = 0
         #kana modify
         self.GPS_error_type = 0
+        self.FIX_error_type = 0
 
-        self.talker(sensor_state, system_state)
+        self.talker(sensor_state, system_state, gps_accuracy)
 
-    def talker(self,sensorstate, systemstate):
+    def talker(self,sensorstate, systemstate, gpsaccuracy):
         print("="*50)
-        print("Camera0, Camera1, LiDAR, IMU, INS, GPSAcc")
+        print("Camera0, Camera1, LiDAR, GPS, INS")
         print(sensorstate.data)
 
         print("Cam_result, LiDAR_result")
@@ -159,6 +148,7 @@ class Final:
         
         self.sensor_check.publish(sensorstate)
         self.system_check.publish(systemstate)
+        self.gps_check.publish(gpsaccuracy)
 
         self.sensor_check.data = []
         self.system_check.data = []
