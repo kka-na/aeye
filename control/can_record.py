35e9a6c5f6b7c4ac85cb013930f459698c4b8e61
Gammacow <hj.han247@gmail.com>
2022-03-25 22:29:15 +0900

    20220325 can_record complete

--- control/can_record.py
+++ control/can_record.py
@@ -36,20 +36,18 @@ class CanReceiver:
         self.brake_pedal = 0
         self.accel_pedal = 0
         self.sas_angle = 0
-
-        self.can_print = True
+        self.can_print = False
 
     def ros_initialize(self):
         rospy.init_node('can_record', anonymous=True)
         rospy.Subscriber("/mode", Int8, self.mode_callback)
-        self.can_record = rospy.Publisher(
-            '/can_record', Int16MultiArray, queue_size=1)
-        self.can_switch = rospy.Publisher(
-            '/can_switch', Int8MultiArray, queue_size=1)
+        self.can_record = rospy.Publisher('/can_record', Int16MultiArray, queue_size=1)
+        self.can_switch = rospy.Publisher( '/can_switch', Int8MultiArray, queue_size=1)
+        self.mode_pub = rospy.Publisher('/mode_set', Int8, queue_size=1)
         self.can_record_data = Int16MultiArray()
-        self.can_record_data = [0, 0, 0]
+        self.can_record_data.data = [0, 0, 0]
         self.can_switch_data = Int8MultiArray()
-        self.can_switch_data = [0, 0, 0]
+        self.can_switch_data.data = [0, 0, 0]
         '''
         record:
         [brake_pedal, accel_pedal, sas_angle]
@@ -58,61 +56,50 @@ class CanReceiver:
         '''
         self.ros_print = True
 
-    def signal_handler(self, sig, frame):
-        sys.exit(0)
 
     def receiver(self):
         while True:
-            try:
-                signal.signal(signal.SIGINT, self.signal_handler)
-                msg = self.bus.recv(0.01)
-                if (msg.arbitration_id == 897):
-                    mdps = self.db.decode_message(msg.arbitration_id, msg.data)
-                    self.drvtq = mdps['CR_Mdps_DrvTq']
-                    if self.can_print:
-                        print(self.drvtq)
+            msg = self.bus.recv(0.01)
+            if (msg.arbitration_id == 897):
+                mdps = self.db.decode_message(msg.arbitration_id, msg.data)
+                self.drvtq = mdps['CR_Mdps_DrvTq']
+                if self.can_print:
+                    print("Drive Torque : {}".format(self.drvtq))
 
-                if (msg.arbitration_id == 881):
-                    eemp = self.db.decode_message(msg.arbitration_id, msg.data)
-                    self.brake_pedal = eemp['SG_Brake_Pedal_Pos']
-                    self.accel_pedal = eemp['SG_Accel_Pedal_Pos']
-                    if self.can_print:
-                        print("Brake Pedal Pos : {}".format(self.brake_pedal))
-                        print("Accel Pedal Pos : {}".format(self.accel_pedal))
+            if (msg.arbitration_id == 881):
+                eemp = self.db.decode_message(msg.arbitration_id, msg.data)
+                self.brake_pedal = eemp['Brake_Pedal_Pos']
+                self.accel_pedal = eemp['Accel_Pedal_Pos']
+                if self.can_print:
+                    print("Brake Pedal Pos : {}".format(self.brake_pedal))
+                    print("Accel Pedal Pos : {}".format(self.accel_pedal))
 
-                if (msg.arbitration_id == 688):
-                    sas = self.db.decode_message(msg.arbitration_id, msg.data)
-                    self.sas_angle = sas['SG_SAS_Angle']
-                    if self.can_print:
-                        print("SAS Angle : ".format(self.sas_angle))
-
-            except Exception as e:
-                exit()
+            if (msg.arbitration_id == 688):
+                sas = self.db.decode_message(msg.arbitration_id, msg.data)
+                self.sas_angle = sas['SAS_Angle']
+                if self.can_print:
+                    print("SAS Angle : ".format(self.sas_angle))
 
     def publisher(self):
-        while not rospy.is_shutdown():
-            try:
-                signal.signal(signal.SIGINT, self.signal_handler)
-                # publish data
-                record, switch = self.calculate_can()
-                self.can_record_data = record
-                self.can_switch_data = switch
-                self.can_record.publish(self.can_record_data)
-                self.can_switch.publish(self.can_switch_data)
+        while True:
+            # publish data
+            record, switch = self.calculate_can()
+            self.can_record_data.data = record
+            self.can_switch_data.data = switch
+            self.can_record.publish(self.can_record_data)
+            self.can_switch.publish(self.can_switch_data)
 
-                if self.ros_print:
-                    print("CAN RECORD")
-                    print(self.can_record_data)
-                    print("CAN SWITCH")
-                    print(self.can_switch_data)
+            if self.ros_print:
+                print("CAN RECORD")
+                print(self.can_record_data)
+                print("CAN SWITCH")
+                print(self.can_switch_data)
+                os.system('clear')
+            rospy.sleep(0.1)
 
-                rospy.sleep(0.5)
-
-            except Exception as e:
-                exit()
 
     def calculate_can(self):
-        record = self.can_record_data
+        record = self.can_record_data.data
         switch = [0, 0, 0]
         '''
         record:
@@ -120,16 +107,19 @@ class CanReceiver:
         switch:
         [drvtq, brake_pedal, accel_pedal]
         '''
-        record[0] = self.brake_pedal
-        record[1] = self.accel_pedal
-        record[2] = self.sas_angle
+        record[0] = int(self.brake_pedal)
+        record[1] = int(self.accel_pedal)
+        record[2] = int(self.drvtq)
 
-        if abs(self.drvtq) > 150 and self.mode == 1:
+        if self.brake_pedal > 30 and self.mode == 1:
             switch[0] = 1
-        elif self.brake_pedal > 20 and self.mode == 1:
-            switch[1] = 1
         elif self.accel_pedal > 30 and self.mode == 1:
+            switch[1] = 1
+        elif abs(self.drvtq) > 150 and self.mode == 1:
             switch[2] = 1
+
+        if 1 in switch:
+            self.mode_pub.publish(0)
 
         return record, switch
 
