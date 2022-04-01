80027388eaf3c921fcf9f03b63d3e07253d49160
Gammacow <hj.han247@gmail.com>
2022-04-01 16:24:15 +0900

    220401 bsd can update

--- control/can_bsd_push.py
+++ control/can_bsd_push.py
@@ -19,35 +19,44 @@ class Can:
     def can_initialize(self):
         self.db = cantools.database.load_file(
             '/home/lattepanda/Documents/can/hyundai_can.dbc')
-        #self.bus1 = can.interface.Bus(
-            #bustype='socketcan', channel='can1', bitrate=500000)
+        # self.bus1 = can.interface.Bus(
+            # bustype='socketcan', channel='can1', bitrate=500000)
         self.bus1 = can.ThreadSafeBus(
-            interface='socketcan', channel='can1', bitrate=500000)
+            interface='socketcan', channel='can0', bitrate=500000)
 
     def ros_initialize(self):
         rospy.Subscriber("/BSD_check", Int8MultiArray, self.BSD_callback)
         self.bsd_array = [0, 0]
 
-    def can_frame_planning(self):
-        if self.bsd_array == [0, 0]:
-            bsd_data = self.db.encode_message('aeye', {'custom_bsd_left':0, 'custom_bsd_right':0})
-            self.bsd_can_msg = can.Message(arbitration_id=1060, data=bsd_data)
-        elif self.bsd_array == [0, 1]:
-            bsd_data = self.db.encode_message('aeye', {'custom_bsd_left':0, 'custom_bsd_right':1})
-            self.bsd_can_msg = can.Message(arbitration_id=1060, data=bsd_data)
-        elif self.bsd_array == [1, 0]:
-            bsd_data = self.db.encode_message('aeye', {'custom_bsd_left':1, 'custom_bsd_right':0})
-            self.bsd_can_msg = can.Message(arbitration_id=1060, data=bsd_data)
-        elif self.bsd_array == [1, 1]:
-            bsd_data = self.db.encode_message('aeye', {'custom_bsd_left':1, 'custom_bsd_right':1})
-            self.bsd_can_msg = can.Message(arbitration_id=1060, data=bsd_data)
-
-    def bsd2can(self):
-        self.can_frame_planning()
-        self.bus1.send(self.bsd_can_msg)
-
     def BSD_callback(self, msg):
         self.bsd_array = msg.data
+        
+    def can_frame_planning(self):
+        bsd_data = self.db.encode_message('aeye', {'custom_bsd_left':self.bsd_array[0], 'custom_bsd_right':self.bsd_array[1]})
+        self.bsd_can_msg = can.Message(arbitration_id=1060, data=bsd_data)
+
+
+        # if self.bsd_array[0] == 0:
+        #     if self.bsd_array[1] == 0:
+        #         bsd_data = self.db.encode_message('aeye', {'custom_bsd_left':0, 'custom_bsd_right':})
+        #         self.bsd_can_msg = can.Message(arbitration_id=1060, data=bsd_data)
+        # elif self.bsd_array[0] == 1:
+        #     print("01")
+        #     bsd_data = self.db.encode_message('aeye', {'custom_bsd_left':1})
+        #     self.bsd_can_msg = can.Message(arbitration_id=1060, data=bsd_data)
+
+        # if self.bsd_array[1] == 0:
+        #     bsd_data = self.db.encode_message('aeye', {'custom_bsd_right':0})
+        #     self.bsd_can_msg = can.Message(arbitration_id=1060, data=bsd_data)
+        # elif self.bsd_array[1] == 1:
+        #     bsd_data = self.db.encode_message('aeye', {'custom_bsd_right':1})
+        #     self.bsd_can_msg = can.Message(arbitration_id=1060, data=bsd_data)
+        
+    def bsd2can(self):
+        self.can_frame_planning()
+        # print(self.bsd_can_msg)
+        self.bus1.send(self.bsd_can_msg)
+
 
 
 def main():
