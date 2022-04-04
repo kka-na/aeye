456921692228476e662db0242a0fb61395bd550c
Kim Kana <kimgana96@gmail.com>
2022-04-05 01:24:21 +0900

    Merge mode change with custom BSD send - Final V1.0

--- control/can_mode.py
+++ control/can_mode.py
@@ -6,20 +6,22 @@ import time
 import binascii                
 import os    
 import rospy 
-from std_msgs.msg import Int8
+from std_msgs.msg import Int8, Int8MultiArray
 
 class OP():
     def __init__(self):
         rospy.init_node('op_mode', anonymous=True)
-        self.bus = can.ThreadSafeBus(interface='socketcan', channel='can0', bitreate=500000)
+        self.bus = can.ThreadSafeBus(interface='socketcan', channel='can1', bitreate=500000)
         self.db = cantools.database.load_file('/home/lattepanda/Documents/can/hyundai_can.dbc')
 
+        self.bsd_array = [0, 0]
         self.radar = {'main' : 0, 'speed' : 0}
         self.acc = False
-        self.spam = {'main' : time.time(), 'accel' : time.time(), 'acc' : time.time()}
+        self.spam = {'main' : time.time(), 'accel' : time.time(), 'acc' : time.time(), 'bsd' : time.time()}
 
         rospy.Subscriber('/mode', Int8, self.mode_callback)
         rospy.Subscriber('/target_vel', Int8, self.speed_callback)
+        rospy.Subscriber("/BSD_check", Int8MultiArray, self.BSD_callback)
 
         self.recv_mode = 0
         self.recv_speed = 0
@@ -29,6 +31,9 @@ class OP():
     
     def speed_callback(self, msg):
         self.recv_speed = msg.data
+
+    def BSD_callback(self, msg):
+        self.bsd_array = msg.data
 
     def receive(self):
         data = self.bus.recv()
@@ -43,8 +48,6 @@ class OP():
             self.acc = True if data['ACCMode'] == "enabled" else False 
         elif (data.arbitration_id == 1265): #Debug
             data = self.db.decode_message(data.arbitration_id, data.data)
-            
-
 
     def send(self, mode, target): #add mode
         data = {'CF_Clu_CruiseSwState': 0.0, 'CF_Clu_CruiseSwMain': 0.0, 'CF_Clu_SldMainSW': 0.0, 'CF_Clu_ParityBit1': 0.0, 'CF_Clu_VanzDecimal': 0.0, 'CF_Clu_Vanz': 0.0, 'CF_Clu_SPEED_UNIT': 0.0, 'CF_Clu_DetentOut': 1.0, 'CF_Clu_RheostatLevel': 12.0, 'CF_Clu_CluInfo': 0.0, 'CF_Clu_AmpInfo': 0.0, 'CF_Clu_AliveCnt1': 0.0}
@@ -75,6 +78,11 @@ class OP():
             if(self.radar['speed'] != target):
                 self.sender(data)
 
+        if(time.time() - self.spam['bsd'] > 0.5):
+            self.spam['bsd'] = time.time()
+            data = {'custom_bsd_left':self.bsd_array[0], 'custom_bsd_right':self.bsd_array[1]}
+            self.sender(data, clu=False)
+
     def target(self, current, target):
         if current < target:
             return 1
@@ -83,9 +91,13 @@ class OP():
         elif current == target:
             return 0
 
-    def sender(self, data):
-        msg = self.db.encode_message('CLU11', data)
-        can_msg = can.Message(arbitration_id=0x4f1, data=msg, is_extended_id=False)
+    def sender(self, data, clu=True):
+        if (clu == True):
+            msg = self.db.encode_message('CLU11', data) #Button Data 
+            can_msg = can.Message(arbitration_id=0x4f1, data=msg, is_extended_id=False)
+        else:
+            msg = self.db.encode_message('aeye', data) #Custom AEye data for BSD
+            can_msg = can.Message(arbitration_id=1060, data=msg, is_extended_id=False)
         self.bus.send(can_msg)
 
     def daemon(self):
