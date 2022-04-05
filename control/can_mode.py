65f7ca82279085be832b2b98807e2ab5d3afc17d
Kim Kana <kimgana96@gmail.com>
2022-04-05 11:14:20 +0900

    Add SAS Angle Longitudinal speed change, mode chan

--- control/can_mode.py
+++ control/can_mode.py
@@ -18,7 +18,10 @@ class OP():
         self.radar = {'main' : 0, 'speed' : 0}
         self.acc = False
         self.spam = {'main' : time.time(), 'accel' : time.time(), 'acc' : time.time(), 'bsd' : time.time()}
+        self.cnt = 0 #For Speed switch ON
+        self.sas_angle = 0
 
+        self.acc_pub = rospy.Publisher("/acc", Int8, queue_size=1)
         rospy.Subscriber('/mode', Int8, self.mode_callback)
         rospy.Subscriber('/target_vel', Int8, self.speed_callback)
         rospy.Subscriber("/BSD_check", Int8MultiArray, self.BSD_callback)
@@ -48,29 +51,38 @@ class OP():
             self.acc = True if data['ACCMode'] == "enabled" else False 
         elif (data.arbitration_id == 1265): #Debug
             data = self.db.decode_message(data.arbitration_id, data.data)
+        elif (msg.arbitration_id == 688):
+                sas = self.db.decode_message(msg.arbitration_id, msg.data)
+                self.sas_angle = sas['SAS_Angle']
 
     def send(self, mode, target): #add mode
         data = {'CF_Clu_CruiseSwState': 0.0, 'CF_Clu_CruiseSwMain': 0.0, 'CF_Clu_SldMainSW': 0.0, 'CF_Clu_ParityBit1': 0.0, 'CF_Clu_VanzDecimal': 0.0, 'CF_Clu_Vanz': 0.0, 'CF_Clu_SPEED_UNIT': 0.0, 'CF_Clu_DetentOut': 1.0, 'CF_Clu_RheostatLevel': 12.0, 'CF_Clu_CluInfo': 0.0, 'CF_Clu_AmpInfo': 0.0, 'CF_Clu_AliveCnt1': 0.0}
 
-        if(self.radar['main'] == 0 and mode == 1 and time.time()-self.spam['main'] > 0.1): #@Zero
+        if(self.radar['main'] == 0 and mode == 1 and time.time()-self.spam['main'] > 0.1): #Main Switch On
             self.spam['main'] = time.time()
             data['CF_Clu_CruiseSwMain'] = 1 
             data['CF_Clu_CruiseSwState'] = 0
             self.sender(data)
 
-        elif(self.radar['main'] == 1 and mode == 0 and time.time()-self.spam['main'] > 0.1): #@Zero
+        elif(self.radar['main'] == 1 and mode == 0 and time.time()-self.spam['main'] > 0.1): #Main Switch Off
             self.spam['main'] = time.time()
             data['CF_Clu_CruiseSwMain'] = 1 
             data['CF_Clu_CruiseSwState'] = 0
             self.sender(data)
 
-        if(self.radar['main'] == 1 and mode == 1 and self.acc == False and time.time()-self.spam['acc'] > 0.1):
+        if(self.radar['main'] == 1 and mode == 1 and self.acc == False and time.time()-self.spam['acc'] > 0.1): #Speed Switch On
             self.spam['acc'] = time.time()
             data['CF_Clu_CruiseSwState'] = 2
             data['CF_Clu_Vanz'] = self.radar['speed']
-            self.sender(data)
+            if(self.cnt < 20):
+                self.sender(data)
+                self.cnt =  0
+                self.acc_pub.publish(1)
+            elif(self.acc == False):
+                self.acc_pub.publish(0)
+                self.cnt =  0
 
-        elif(self.radar['main'] == 1 and mode == 1 and time.time()-self.spam['accel'] > 0.1 and self.acc == True):
+        elif(self.radar['main'] == 1 and mode == 1 and time.time()-self.spam['accel'] > 0.1 and self.acc == True): #Goto target
             self.spam['accel'] = time.time()
             data['CF_Clu_CruiseSwState'] = self.target(self.radar['speed'], target)
             data['CF_Clu_CruiseSwMain'] = 0
@@ -78,12 +90,15 @@ class OP():
             if(self.radar['speed'] != target):
                 self.sender(data)
 
-        if(time.time() - self.spam['bsd'] > 0.5):
+
+        if(time.time() - self.spam['bsd'] > 0.5): #BSD
             self.spam['bsd'] = time.time()
             data = {'custom_bsd_left':self.bsd_array[0], 'custom_bsd_right':self.bsd_array[1]}
             self.sender(data, clu=False)
 
     def target(self, current, target):
+        if(self.sas_angle > 10):
+            target = min(target, target - int(abs(self.sas_angle)/20))
         if current < target:
             return 1
         elif current > target:
