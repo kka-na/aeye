90dbda150a85e902db423e6d56b5408ea00530f2
kka-na <kimgana96@gmail.com>
2022-04-29 16:50:03 +0900

    Update LaneLines Display Code

--- utils/laneline_display.py
+++ utils/laneline_display.py
@@ -1,9 +1,6 @@
-import os
-import sys
-from matplotlib.style import context
 import zmq
-import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
 from cereal.messaging import sub_sock, recv_one_or_none, SubMaster
 from cereal.services import services
@@ -19,11 +16,25 @@ frame.setsockopt(zmq.SUBSCRIBE, "")
 modelv2 = sub_sock(context, services['modelV2'].port, addr=addr, conflate=True)
 md2 = recv_one_or_none(modelv2)
 if md2:
+    x = md2.modelV2.laneLines[1].x
     line1s = md2.modelV2.laneLines[1].y
     line2s = md2.modelV2.laneLines[2].y
+
+    plt.ion()
+    animated_plot = plt.plot(x, line1s, 'ro', x, line2s, 'ro')[0]
+
+    for i in range(len(x)):
+        animated_plot.set_xdata(x[0:i])
+        animated_plot.set_y1data(line1s[0:i])
+        animated_plot.set_y2data(line2s[0:i])
+        plt.draw()
+        plt.pause(0.1)
+
+    '''
     print("Line1 : ")
     for point in line1s:
         print("{} ".format(point))
     print("\nLine2 : ")
     for point in line2s:
         print("{} ".format(point))
+    '''
