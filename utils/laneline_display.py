37c91a6206669731c865f5f325ee19c246aea5ab
kka-na <kimgana96@gmail.com>
2022-05-04 18:08:38 +0900

    LaneLine Display with Driver Camera Test

--- utils/laneline_display.py
+++ utils/laneline_display.py
@@ -1,53 +1,59 @@
+from cereal.messaging import SubMaster
 from turtle import left
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 from cereal.visionipc.visionipc_pyx import VisionIpcClient, VisionStreamType
+import os
+from ui_helpers import _BB_TO_FULL_FRAME
 
-from cereal.messaging import SubMaster
+os.environ["ZMQ"] = "1"
+
 
 sm = SubMaster(['carState', 'longitudinalPlan', 'carControl', 'radarState', 'liveCalibration', 'controlsState', 'carParams',
-                            'liveTracks', 'modelV2', 'liveParameters', 'lateralPlan', 'sendcan', 'gpsLocationExternal', 
-                            'clocks','thumbnail', 'roadCameraState', 'driverState', 'procLog', 'ubloxGnss', 'ubloxRaw', 
-                            'cameraOdometry', 'carEvents', 'driverCameraState', 'driverMonitoringState'], 
-                            addr='192.168.43.1')
+                            'liveTracks', 'modelV2', 'liveParameters', 'lateralPlan', 'sendcan', 'gpsLocationExternal',
+                            'clocks', 'thumbnail', 'roadCameraState', 'driverState', 'procLog', 'ubloxGnss', 'ubloxRaw',
+                            'cameraOdometry', 'carEvents', 'driverCameraState', 'driverMonitoringState'],
+               addr='192.168.43.1')
 
-temp = {'0' : 0, '1' : 0, '2' : 0, '3' : 0}
+temp = {'0': 0, '1': 0, '2': 0, '3': 0}
 
 img = np.zeros((480, 640, 3), dtype='uint8')
 imgff = None
 num_px = 0
-vipc_client = VisionIpcClient("camerad", VisionStreamType.VISION_STREAM_RGB_ROAD, True)
+vipc_client = VisionIpcClient(
+    "camerad", VisionStreamType.VISION_STREAM_RGB_ROAD, True)
 
 while 1:
-    # if not vipc_client.is_connected():
-    #   vipc_client.connect(True)
+    if not vipc_client.is_connected():
+        vipc_client.connect(True)
+    rgb_img_raw = vipc_client.recv()
+    if rgb_img_raw is None or not rgb_img_raw.any():
+        continue
+    num_px = len(rgb_img_raw) // 3
+    imgff_shape = (vipc_client.height, vipc_client.width, 3)
+    if imgff is None or imgff.shape != imgff_shape:
+        imgff = np.zeros(imgff_shape, dtype=np.uint8)
+    imgff = np.frombuffer(rgb_img_raw, dtype=np.uint8).reshape(
+        (vipc_client.height, vipc_client.width, 3))
+    imgff = imgff[:, :, ::-1]  # Convert BGR to RGB
 
-    # rgb_img_raw = vipc_client.recv()
+    zoom_matrix = _BB_TO_FULL_FRAME[num_px]
+    cv2.warpAffine(imgff, zoom_matrix[:2], (img.shape[1],
+                   img.shape[0]), dst=img, flags=cv2.WARP_INVERSE_MAP)
 
-    # if rgb_img_raw is None or not rgb_img_raw.any():
-    #   continue
+    cv2.imshow("img", img)
+    cv2.waitKey(30)
 
-    # num_px = len(rgb_img_raw) // 3
-    # imgff_shape = (vipc_client.height, vipc_client.width, 3)
-
-    # if imgff is None or imgff.shape != imgff_shape:
-    #   imgff = np.zeros(imgff_shape, dtype=np.uint8)
-
-    # imgff = np.frombuffer(rgb_img_raw, dtype=np.uint8).reshape((vipc_client.height, vipc_client.width, 3))
-    # imgff = imgff[:, :, ::-1]  # Convert BGR to RGB
-    
-    #cv2.imshow("Test", imgff)
-    
     sm.update(0)
 
     if sm['modelV2']:
         if sm['modelV2'].laneLines:
             for i, _ in enumerate(sm['modelV2'].laneLines):
-                temp[str(i)] =  _
-            
+                temp[str(i)] = _
+
             x = np.uint32(temp['1'].x)
-            
+
             line0s = temp['0'].y
             line1s = temp['1'].y
             line2s = temp['2'].y
@@ -64,7 +70,7 @@ while 1:
             # if (line2s[0]<0.7):
             #     print("Lane 2 is Near 0")
             #     bad = True
-            
+
             # if(not bad):
             #    print("State OK")
             # else:
@@ -77,30 +83,34 @@ while 1:
             #     print("left out")
             # else:
             #     print("left stable")
-            
+
             # if (0.75<line2s[0]<0.9):
             #     print("right warning")
             # elif (0.75>line2s[0]):
             #     print("right out")
             # else:
             #     print("right stable")
-            
+
             xx = np.array(x)[5:15]
-            
+
             lefty = np.array(line1s)[5:15]
             righty = np.array(line2s)[5:15]
             left_fit_cr = np.polyfit(xx, lefty, 2)
             right_fit_cr = np.polyfit(xx, righty, 2)
             # print(left_fit_cr[0], left_fit_cr[1], left_fit_cr[2])
             # print(right_fit_cr[0], right_fit_cr[1], right_fit_cr[2])
-            left_curvated = ((1+(2*left_fit_cr[0]+left_fit_cr[1])**2)**1.5)/np.absolute(2*left_fit_cr[0])
-            right_curvated = ((1+(2*right_fit_cr[0]+right_fit_cr[1])**2)**1.5)/np.absolute(2*right_fit_cr[0])
+            left_curvated = (
+                (1+(2*left_fit_cr[0]+left_fit_cr[1])**2)**1.5)/np.absolute(2*left_fit_cr[0])
+            right_curvated = (
+                (1+(2*right_fit_cr[0]+right_fit_cr[1])**2)**1.5)/np.absolute(2*right_fit_cr[0])
 
-            if(int(left_curvated) < 300 ):
+            if(int(left_curvated) < 300):
                 #print("Left Cur : {}m , Right Cur : {}m".format(int(left_curvated), int(right_curvated)))
-                print("Can't Change Lanes Left Cur : {} ".format(int(left_curvated)))
+                print("Can't Change Lanes Left Cur : {} ".format(
+                    int(left_curvated)))
             if(int(right_curvated) < 300):
-                print("Can't Change Lanes Right Cur : {} ".format(int(right_curvated)))
+                print("Can't Change Lanes Right Cur : {} ".format(
+                    int(right_curvated)))
 
             '''
             if left_blink:
@@ -113,7 +123,8 @@ while 1:
             plt.figure
             plt.xlim(-6, 6)
             plt.ylim(0, 50)
-            animated_plot = plt.plot(line0s, x, 'bo', line1s, x, 'ro', line2s, x, 'ro', line3s, x, 'bo', linestyle='--')[0]
+            animated_plot = plt.plot(
+                line0s, x, 'bo', line1s, x, 'ro', line2s, x, 'ro', line3s, x, 'bo', linestyle='--')[0]
 
             plt.draw()
             plt.pause(0.1)
--- utils/test.py
+++
@@ -1,2 +0,0 @@
-import cereal.messaging as messaging
-
