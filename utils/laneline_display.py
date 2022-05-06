94e3f5ef264b295ad866ecbff77d23f6bf71ef5c
Kim Kana <kimgana96@gmail.com>
2022-05-06 17:10:26 +0900

    LaneLine Display Update

--- utils/laneline_display.py
+++ utils/laneline_display.py
@@ -1,51 +1,36 @@
 from cereal.messaging import SubMaster
-from turtle import left
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
-from cereal.visionipc.visionipc_pyx import VisionIpcClient, VisionStreamType
 import os
-from ui_helpers import _BB_TO_FULL_FRAME
-
 os.environ["ZMQ"] = "1"
 
-
+addr = '192.168.1.6'
 sm = SubMaster(['carState', 'longitudinalPlan', 'carControl', 'radarState', 'liveCalibration', 'controlsState', 'carParams',
                             'liveTracks', 'modelV2', 'liveParameters', 'lateralPlan', 'sendcan', 'gpsLocationExternal',
                             'clocks', 'thumbnail', 'roadCameraState', 'driverState', 'procLog', 'ubloxGnss', 'ubloxRaw',
-                            'cameraOdometry', 'carEvents', 'driverCameraState', 'driverMonitoringState'],
-               addr='192.168.43.1')
+                            'cameraOdometry', 'carEvents', 'driverCameraState',  'wideRoadCameraState', 'driverMonitoringState'],
+               addr=addr)
 
 temp = {'0': 0, '1': 0, '2': 0, '3': 0}
 
-img = np.zeros((480, 640, 3), dtype='uint8')
-imgff = None
-num_px = 0
-vipc_client = VisionIpcClient(
-    "camerad", VisionStreamType.VISION_STREAM_RGB_ROAD, True)
+H = 1208//2
+W =1928//2
 
 while 1:
-    if not vipc_client.is_connected():
-        vipc_client.connect(True)
-    rgb_img_raw = vipc_client.recv()
-    if rgb_img_raw is None or not rgb_img_raw.any():
-        continue
-    num_px = len(rgb_img_raw) // 3
-    imgff_shape = (vipc_client.height, vipc_client.width, 3)
-    if imgff is None or imgff.shape != imgff_shape:
-        imgff = np.zeros(imgff_shape, dtype=np.uint8)
-    imgff = np.frombuffer(rgb_img_raw, dtype=np.uint8).reshape(
-        (vipc_client.height, vipc_client.width, 3))
-    imgff = imgff[:, :, ::-1]  # Convert BGR to RGB
-
-    zoom_matrix = _BB_TO_FULL_FRAME[num_px]
-    cv2.warpAffine(imgff, zoom_matrix[:2], (img.shape[1],
-                   img.shape[0]), dst=img, flags=cv2.WARP_INVERSE_MAP)
-
-    cv2.imshow("img", img)
-    cv2.waitKey(30)
-
     sm.update(0)
+    
+    # 1. Road Camera streaming Test
+    '''
+    if sm.updated['roadCameraState']:
+        bgr_raw = sm['roadCameraState'].image
+        # print(len(bgr_raw))
+        dat = np.frombuffer(bgr_raw, dtype=np.uint8).reshape(H, W, 3)
+        avg = ticker - time.time() + avg / 2
+        ticker = time.time()
+        cv2.imshow('frame', dat)
+        cv2.waitKey(1)
+    '''
 
     if sm['modelV2']:
         if sm['modelV2'].laneLines:
@@ -53,71 +38,67 @@ while 1:
                 temp[str(i)] = _
 
             x = np.uint32(temp['1'].x)
-
             line0s = temp['0'].y
             line1s = temp['1'].y
             line2s = temp['2'].y
             line3s = temp['3'].y
 
-            bad = False
+            # 2. Calculate Whether a Car is on the Lane or Not
+            '''
+            onLane = False
+            if (line2s[0]-line1s[0] > 3.6):
+                print("Lane 1 & 2's Width {} ".format(line2s[0]-line1s[0]))
+                onLane = True
+            if (line1s[0]>-0.7):
+                print("Lane 1 is Near 0")
+                onLane = True
+            if (line2s[0]<0.7):
+                print("Lane 2 is Near 0")
+                onLane = True
 
-            # if (line2s[0]-line1s[0] > 3.6):
-            #     print("Lane 1 & 2's Width {} ".format(line2s[0]-line1s[0]))
-            #     bad = True
-            # if (line1s[0]>-0.7):
-            #     print("Lane 1 is Near 0")
-            #     bad = True
-            # if (line2s[0]<0.7):
-            #     print("Lane 2 is Near 0")
-            #     bad = True
+            if(not onLane):
+               print("Car is Keep in the Lane")
+            else:
+               print("Car is on the Lane")
+            '''
 
-            # if(not bad):
-            #    print("State OK")
-            # else:
-            #    print("State BAD")
-            # print("left: ", line1s[0])
-            # print("right: ", line2s[0])
-            # if (-0.9<line1s[0]<-0.75):
-            #     print("left warning")
-            # elif (-0.75<line1s[0]):
-            #     print("left out")
-            # else:
-            #     print("left stable")
 
-            # if (0.75<line2s[0]<0.9):
-            #     print("right warning")
-            # elif (0.75>line2s[0]):
-            #     print("right out")
-            # else:
-            #     print("right stable")
+            # 3. Calculate Lane Departure
+            '''
+            if (-0.9<line1s[0]<-0.75):
+                print("Left Warning")
+            elif (-0.75<line1s[0]):
+                print("Left Out")
+            else:
+                print("Left Stable")
 
-            xx = np.array(x)[5:15]
+            if (0.75<line2s[0]<0.9):
+                print("Right Warning")
+            elif (0.75>line2s[0]):
+                print("Right Out")
+            else:
+                print("Right Stable")
+            '''
 
+
+            # 4. Calculate Curvature 
+            '''
+            xx = np.array(x)[5:15] # for calculate forward lane (5~15)
             lefty = np.array(line1s)[5:15]
             righty = np.array(line2s)[5:15]
-            left_fit_cr = np.polyfit(xx, lefty, 2)
+            left_fit_cr = np.polyfit(xx, lefty, 2) # return poltnomial coefficient
             right_fit_cr = np.polyfit(xx, righty, 2)
-            # print(left_fit_cr[0], left_fit_cr[1], left_fit_cr[2])
-            # print(right_fit_cr[0], right_fit_cr[1], right_fit_cr[2])
-            left_curvated = (
-                (1+(2*left_fit_cr[0]+left_fit_cr[1])**2)**1.5)/np.absolute(2*left_fit_cr[0])
-            right_curvated = (
-                (1+(2*right_fit_cr[0]+right_fit_cr[1])**2)**1.5)/np.absolute(2*right_fit_cr[0])
+            left_curvated = ((1+(2*left_fit_cr[0]+left_fit_cr[1])**2)**1.5)/np.absolute(2*left_fit_cr[0]) # calculate curvature
+            right_curvated = ((1+(2*right_fit_cr[0]+right_fit_cr[1])**2)**1.5)/np.absolute(2*right_fit_cr[0])
 
             if(int(left_curvated) < 300):
-                #print("Left Cur : {}m , Right Cur : {}m".format(int(left_curvated), int(right_curvated)))
-                print("Can't Change Lanes Left Cur : {} ".format(
-                    int(left_curvated)))
+                print("Can't Change Lanes Left Cur : {} ".format(int(left_curvated)))
             if(int(right_curvated) < 300):
-                print("Can't Change Lanes Right Cur : {} ".format(
-                    int(right_curvated)))
-
-            '''
-            if left_blink:
-                if left_curvature < 1000 : # not straight line 
-                    do not change lane 
+                print("Can't Change Lanes Right Cur : {} ".format(int(right_curvated)))
             '''
 
+
+            # 5. Plotting Lanes 0,1,2,3
             plt.ion()
             plt.cla()
             plt.figure
