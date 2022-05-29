69249395f64c6a7ee1cb5a85e38be15c8a854295
Hyung Jin Justin Han <hjhan@inha.edu>
2022-05-30 01:17:31 +0900

    id2name, name2id prints signals

--- analysis/tools/id2name.py
+++ analysis/tools/id2name.py
@@ -1,9 +1,17 @@
 import cankey
+import cantools
+import pprint
 
 if __name__ == '__main__':
+    db = cantools.database.load_file('../data/hyundai_can.dbc')
     while 1:
         id = int(input('Enter ID : '))
         if id in cankey.ID:
-            print(cankey.ID[id])
+            name = cankey.ID[id]
+            msg = db.decode_message(name, [0 for _ in range(cankey.DLC[id])])
+            print(name)
+            pprint.pprint(msg)
+            print('____________________\n)')
+            
         else:
             print('No such id')
--- analysis/tools/name2id.py
+++ analysis/tools/name2id.py
@@ -1,11 +1,23 @@
 import cankey
+import cantools
+import pprint
 
 if __name__ == '__main__':
+    db = cantools.database.load_file('../data/hyundai_can.dbc')
+
     while 1:
         name = input('Enter name : ')
         name = name.upper()
 
         if name in cankey.NAME:
-            print(cankey.NAME[name])
+            id = cankey.NAME[name]
+            msg = db.decode_message(name, [0 for _ in range(cankey.DLC[id])])
+            print(id)
+            pprint.pprint(msg)
+            print('____________________\n')
+
+
+
+
         else:
             print('No such name')
