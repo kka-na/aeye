5304401e60e649b7b210ff5ef63c0658d20b2931
NUC i7 <hjhan@inha.edu>
2022-06-14 18:43:43 +0900

    SH modify

--- sh/can_record.sh
+++ sh/can_record.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-cd /home/Documents/bag && rosbag record /mode /can_record /tor_record
+source /opt/ros/melodic/setup.bash
+export ROS_MASTER_URI="http://192.168.101.1:11311"
+export ROS_HOSTNAME=192.168.101.6
+export ZMQ=1
+
+cd /home/aeye/Documents/bag && rosbag record /mode /can_record /tor_record
+
